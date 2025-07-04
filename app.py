99fe8ed510a9d2bb5207912083b6ab5b2c8e8ed8
eliaw <elia.waefler@gmail.com>
2025-07-04 10:27:43 +0200

    speckle_model

--- app.py
+++ app.py
@@ -9,8 +9,10 @@ def start_page():
     st.set_page_config("speckle test", ":arrow:")
     st.title("welcome")
     st.markdown("""
-    <iframe title="Speckle" src="https://app.speckle.systems/projects/f1363936ee/models/09ebe7db28#embed=%7B%22isEnabled%22%3Atrue%7D" width="600" height="400" frameborder="0"></iframe>
-    """, unsafe_allow_html=True)
+    <iframe title="Speckle" 
+    src="https://app.speckle.systems/projects/f1363936ee/models/09ebe7db28#embed=%7B%22isEnabled%22%3Atrue%2C%22isTransparent%22%3Atrue%2C%22hideControls%22%3Atrue%2C%22hideSelectionInfo%22%3Atrue%2C%22disableModelLink%22%3Atrue%7D" 
+    width="600" height="400" frameborder="0"></iframe>""",
+                unsafe_allow_html=True)
 
 if __name__ == '__main__':
     start_page()
