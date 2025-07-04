5b931f974a2614af1fbf730653289d62e5bf507c
eliaw <elia.waefler@gmail.com>
2025-07-04 10:26:01 +0200

    speckle_model

--- app.py
+++ app.py
@@ -6,11 +6,11 @@ import plotly
 
 
 def start_page():
-    st.set_page_config("spekcle test", ":arrow:")
+    st.set_page_config("speckle test", ":arrow:")
     st.title("welcome")
     st.markdown("""
     <iframe title="Speckle" src="https://app.speckle.systems/projects/f1363936ee/models/09ebe7db28#embed=%7B%22isEnabled%22%3Atrue%7D" width="600" height="400" frameborder="0"></iframe>
-    """)
+    """, unsafe_allow_html=True)
 
 if __name__ == '__main__':
     start_page()
